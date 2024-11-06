2e2161005970cae5d81d9db9f75b9b06e03e5545
Leonardo Carvalho <leo_1497@hotmail.com>
2024-11-06 17:44:06 -0300

    chore: final da config de banco e mudança para sequelize

--- src/server/config/dbconfig.ts
+++ src/server/config/dbconfig.ts
@@ -15,29 +15,32 @@ interface DatabaseConfig {
     };
 }
 
-export const configPrd: DatabaseConfig = {
-    dialect: 'mysql',
-    host: process.env.DB_HOST_PRD || '',
-    username: process.env.DB_USER_PRD || '',
-    password: process.env.DB_PASS_PRD || '',
-    database: process.env.DB_NAME_PRD || '',
-    define: {
-        timestamps: true,
-        underscored: true,
-    }
-};
-
-export const configDev: DatabaseConfig = {
-    dialect: 'mysql',
-    host: process.env.DB_HOST_DEV || '',
-    username: process.env.DB_USER_DEV || '',
-    password: process.env.DB_PASS_DEV || '',
-    database: process.env.DB_NAME_DEV || '',
-    define: {
-        timestamps: true,
-        underscored: true,
-    }
-};
+class DataBase {
+    public configPrd: DatabaseConfig = {
+        dialect: 'mysql',
+        host: process.env.DB_HOST_PRD || '',
+        username: process.env.DB_USER_PRD || '',
+        password: process.env.DB_PASS_PRD || '',
+        database: process.env.DB_NAME_PRD || '',
+        define: {
+            timestamps: true,
+            underscored: true,
+        }
+    };
+    
+    public configDev: DatabaseConfig = {
+        dialect: 'mysql',
+        host: process.env.DB_HOST_DEV || '',
+        username: process.env.DB_USER_DEV || '',
+        password: process.env.DB_PASS_DEV || '',
+        database: process.env.DB_NAME_DEV || '',
+        define: {
+            timestamps: true,
+            underscored: true,
+        }
+    };
+}
 
 // export default { configDev, configPrd };
-module.exports = { configDev, configPrd };
+// module.exports = { configDev, configPrd };
+export default  DataBase
