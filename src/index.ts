c8dec9b9ba6e320456bff3c6c6643865755eb015
Leonardo Carvalho <leo_1497@hotmail.com>
2024-11-07 09:48:20 -0300

    feat: package.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { server } from './server/Server';
 
+const port = process.env.PORT || 3333;
 
-server.listen(process.env.PORT || 3333, () => console.log('App rodando!'));
+server.listen(process.env.PORT || 3333, () => console.log(`App rodando na porta ${port}!`));
