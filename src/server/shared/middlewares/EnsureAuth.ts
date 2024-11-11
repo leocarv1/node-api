806188a4b6b72687c4a3382a0d6be5b8ad35cad7
Leonardo Carvalho <leo_1497@hotmail.com>
2024-11-10 23:28:01 -0300

    fix: correção do auth

--- src/server/shared/middlewares/EnsureAuth.ts
+++ src/server/shared/middlewares/EnsureAuth.ts
@@ -3,12 +3,13 @@ import { Authentication } from "../services/Authentication";
 
 export const ensureAuth = (req: any, res: any, next: any) => {
     const { authorization } = req.headers;
+    console.log(authorization);
     
-    if (!authorization) return res.status(StatusCodes.UNAUTHORIZED).json({ msg: "Unauthorized" });
+    if (!authorization) return res.status(StatusCodes.UNAUTHORIZED).json({ msg: "Unauthorized | no token" });
         
     const [type, token] = authorization.split(' ');
 
-    if (type !== 'Bearer') return res.status(StatusCodes.UNAUTHORIZED).json({ msg: "Unauthorized" });
+    if (type !== 'Bearer') return res.status(StatusCodes.UNAUTHORIZED).json({ msg: "Unauthorized type" });
 
     const jwtData = Authentication.verifyToken(token);
 
@@ -17,7 +18,7 @@ export const ensureAuth = (req: any, res: any, next: any) => {
     if (jwtData === 'JWT_SECRET_NOT_FOUND') {
         return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Invalid secret key" });
     } else if (jwtData === 'INVALID_TOKEN' || !jwtData || !jwtData.uid) {
-        return res.status(StatusCodes.UNAUTHORIZED).json({ msg: "Unauthorized" });
+        return res.status(StatusCodes.UNAUTHORIZED).json({ msg: "Unauthorized | invalid token" });
     }
 
     console.log(jwtData);
